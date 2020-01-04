3b35746da07466fbd86900a16d4032441470d09d
blakelower <blakelower1999@gmail.com>
2020-01-04 13:53:45 +0700

    added Main

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <h1>The Main Component</h1>
+      </div>
+    );
+  }
+}
+export default Main;
