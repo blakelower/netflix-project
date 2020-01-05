795735e3be923908572851adfb6a3f90931dd3bb
blakelower <blakelower1999@gmail.com>
2020-01-05 13:57:20 +0700

    added tabs

--- src/components/TabComponent.js
+++ src/components/TabComponent.js
@@ -6,14 +6,24 @@ import TabPrices from '../components/tab_nav/TabPrices'
 import '../css/TabsNav.css';
 
 class TabComponent extends Component {
+    state = {
+        tabIndex: 0
+    }
     render(){
         return(
             <div>
-                <Tabs>
-                    <TabList className="tab-list">
-                        <Tab> <TabDoor/></Tab>
-                        <Tab><TabDevices/></Tab>
-                        <Tab><TabPrices/></Tab>
+                <Tabs className="tabs" selectedIndex={this.state.tabIndex} onSelect={tabIndex => this.setState({tabIndex})}> 
+                    <TabList className="tab-nav-container">
+                        <Tab className={`${this.state.tabIndex === 0 ? 'active' : null}`}><TabDoor/>
+                        <p><strong>No Commitments<br/>
+                        Cancel online at anytime</strong></p>
+                        </Tab>
+                        <Tab className={`${this.state.tabIndex === 1 ? 'active' : null}`}><TabDevices/>
+                        <p style={{marginTop: '-5.3125rem'}}><strong>Watch Anywhere</strong></p>
+                        </Tab>
+                        <Tab className={`${this.state.tabIndex === 2 ? 'active' : null}`}><TabPrices/>
+                        <p><strong>Pick your Price</strong></p>
+                        </Tab>
                     </TabList>
                 </Tabs>
             </div>
