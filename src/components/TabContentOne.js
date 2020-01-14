f5cf36b99922dc7187d21b18206d736a62654374
blakelower <blakelower1999@gmail.com>
2020-01-14 23:03:22 +0700

    fixed some styling

--- src/components/TabContentOne.js
+++ src/components/TabContentOne.js
@@ -1,66 +1,71 @@
-import React from 'react';
-import Img from '../images/tab-1-pic.png';
-import styled from 'styled-components';
-import{Button } from './Button'
-import {Link } from 'react-router-dom';
-import {generateMedia} from 'styled-media-query'
+import React from "react";
+import Img from "../images/tab-1-pic.png";
+import styled from "styled-components";
+import { Button } from "./Button";
+import { Link } from "react-router-dom";
+import { generateMedia } from "styled-media-query";
 
-function TabContentOne(){
-    return(
-        <TabContentContainer>
-            <div className="container"></div>
-            <div className="tab-content">
-                <div>
-                <span className="title" style={{marginBottom: '2rem'}}>
-                    If you decided that Netflix isn't for you - no problem. No Commitments. Cancel online anytime. 
-                </span>
-                <br /> 
-                <Link to="/chooseplan">
-                <Button style={{marginTop: '2rem'}}>Try It Now</Button></Link>
-                </div>
-                <img src={Img} />
-            </div>
-        </TabContentContainer>
-    )
+function TabContentOne() {
+  return (
+    <TabContentContainer>
+      <div className="container"></div>
+      <div className="tab-content">
+        <div>
+          <span className="title" style={{ marginBottom: "2rem" }}>
+            If you decided that Netflix isn't for you - no problem. No
+            Commitments. Cancel online anytime.
+          </span>
+          <br />
+          <Link to="/chooseplan">
+            <Button style={{ marginTop: "2rem" }}>Try It Now</Button>
+          </Link>
+        </div>
+        <img src={Img} />
+      </div>
+    </TabContentContainer>
+  );
 }
-export default TabContentOne
+export default TabContentOne;
 
 //Media Query
 const customMedia = generateMedia({
-    smDesktop: '1440px',
-    tablet: '960px'
-})
+  smDesktop: "1440px",
+  tablet: "960px"
+});
 
 //Main Content One
 const TabContentContainer = styled.div`
-    background: var(--main-deep-dark);
+  background: var(--main-deep-dark);
 
-    .container{
-        margin: 0 10%;
-    }
+  .container {
+    margin: 0 10%;
+  }
 
-    img{
-        width: 31.875rem;
-    }
-.title{
+  img {
+    width: 31.875rem;
+    ${customMedia.lessThan("smDesktop")`
+        width: 23rem;
+        `}
+  }
+  .title {
     margin-top: 2rem;
-    ${customMedia.lessThan('smDesktop')`
+    ${customMedia.lessThan("smDesktop")`
     font-size: 1.5rem;
     line-height: 1;
     `}
-}
-    .tab-content{
-        display: grid;
-        grid-template-columns: repeat(2, 1fr);
-        grid-gap: 2rem;
-        align-items: center;
-        font-size: 2rem;
-        padding: 2.5rem;
-        ${customMedia.lessThan('tablet')`
+  }
+  .tab-content {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 2rem;
+    align-items: center;
+    font-size: 2rem;
+    padding: 2.5rem;
+    ${customMedia.lessThan("smDesktop")`
         grid-template-columns: 100%;     
         text-align: center;
         padding-left: 0;
         padding: right: 0; 
     `}
-    }
+  }
 `;
